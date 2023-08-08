7f1e9ec79f052074d3665329f3ebf95f6bdf0906
KrieqqeR <burakcevizli@gmail.com>
2023-08-09 00:50:17 +0300

    Listeye ekleme başarılı oldu.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Form from './Components/Form';
 
 function App() {
   const arrayMembers = [
-    { id: 1, isim: "Burak", email: "burakcevizli@gmail.com", rol: "Fullstack Developer" },
-    { id: 2, isim: "Ali", email: "aliköksel@gmail.com", rol: "Frontend Developer" },
-    { id: 3, isim: "Veli", email: "velikaymak@gmail.com", rol: "Backend Developer" },
-    { id: 4, isim: "Süleyman", email: "süleymansoylu@gmail.com", rol: "Jr.Tester" },
+    { isim: "Burak", email: "burakcevizli@gmail.com", rol: "Fullstack Developer" },
+    { isim: "Ali", email: "aliköksel@gmail.com", rol: "Frontend Developer" },
+    { isim: "Veli", email: "velikaymak@gmail.com", rol: "Backend Developer" },
+    { isim: "Süleyman", email: "süleymansoylu@gmail.com", rol: "Jr.Tester" },
   ];
 
   const [members, setMembers] = useState(arrayMembers);
@@ -16,13 +16,15 @@ function App() {
     setMembers([...members, member]);
   };
 
+
+  
   return (
     <div className="App">
       <ul>
         {members.map((member) => (
-          <ol key={member.id}>
+          <li key={member.id}>
             {member.isim} ------ {member.rol}
-          </ol>
+          </li>
         ))}
       </ul>
       <Form addMembers={addMembers} />
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,31 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = (props) => {
-    
-   
+  const [isim, setİsim] = useState("");
+  const [email, setEmail] = useState("");
+  const [rol, setRol] = useState("");
 
-    return (
+  const onChangeHandler = (event) => {
+    const { id, value } = event.target;
+    if (id === "isim") {
+      setİsim(value);
+    } else if (id === "email") {
+      setEmail(value);
+    } else {
+      setRol(value);
+    }
+  };
 
-        <form >
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const newMember = { isim: isim, email: email, rol: rol };
+    props.addMembers(newMember);
 
-        <label htmlFor="isim">İsim</label>
-        <hr />
-        <input id="isim" type="text" />
-        <hr />
-        <label htmlFor="email">Email</label>
-        <hr />
-        <input id="email" type="email" />
-        <hr />
-        <label htmlFor="rol">Rol</label>
-        <hr />
-        <input id="rol" type="text" />
-        <button type="submit">Gönder</button>
+    setİsim("");
+    setEmail("");
+    setRol("");
+  };
 
-        </form>
-
-    )
-
-}
-
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <label htmlFor="isim">İsim</label>
+      <hr />
+      <input id="isim" type="text" value={isim} onChange={onChangeHandler} />
+      <hr />
+      <label htmlFor="email" value={email}>
+        Email
+      </label>
+      <hr />
+      <input id="email" type="email" onChange={onChangeHandler} />
+      <hr />
+      <label htmlFor="rol">Rol</label>
+      <hr />
+      <input id="rol" type="text" onChange={onChangeHandler} />
+      <button type="submit">Gönder</button>
+    </form>
+  );
+};
 
 export default Form;
